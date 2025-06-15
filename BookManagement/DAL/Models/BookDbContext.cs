3c17ee0568e2fa738df7c00f58ba302b23c0e67b
Phat-Hy <hyphat2942004@gmail.com>
2025-06-16 03:36:30 +0700

    Base Code

---
+++ BookManagement/DAL/Models/BookDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Models
+{
+    public class BookDbContext : DbContext
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<UserBookCollection> UserBookCollections { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Collections)
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.UserCollections)
+                .WithOne(uc => uc.Book)
+                .HasForeignKey(uc => uc.BookId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+            modelBuilder.Entity<UserBookCollection>()
+                .Property(uc => uc.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        }
+    }
+}
